035623d93b5a7862280862c6b774f55c56eb821f
clementcisterne <lamouche444@hotmail.fr>
2016-10-07 01:21:57 +0200

    b

--- DessinFractale.java
+++ DessinFractale.java
@@ -1,8 +1,10 @@
+import java.math.* ;
+
 class DessinFractale{
     private final Turtle bob;
     
-    private final static int LARGEUR = 800;
-    private final static int HAUTEUR = 600;
+    private final static int LARGEUR = 1000;
+    private final static int HAUTEUR = 800;
     //taille de la fenetre graphique
 
     public DessinFractale(){
@@ -41,11 +43,11 @@ class DessinFractale{
 
     public void triangle(double l){
     	bob.forward(l);
-		bob.left(120);
+		bob.left(180-(180/3));
     	bob.forward(l);
-		bob.left(120);
+		bob.left(180-(180/3));
 		bob.forward(l);
-		bob.left(120);    
+		bob.left(120);
     }
 
     /**
@@ -90,7 +92,7 @@ class DessinFractale{
 		bob.forward(l);
     		bob.left(180);	
 		bob.forward(l);		
-    		bob.left(150);	
+    		bob.left(150);
     	}
     	else{
     		bob.forward(l);    	
@@ -99,18 +101,141 @@ class DessinFractale{
     		this.arbres(l/2,n-1);
     		this.arbres(l/2,n-1);
     		this.arbres(l/2,n-1);
-            this.arbres(l/2,n-1);
-            this.arbres(l/2,n-1);            
+          	
 		bob.right(120);	
 		bob.forward(l/(13/10));
 		bob.left(150);
     	}
     }
 
-    public static void main(String[] args){
-	DessinFractale d = new DessinFractale(10);
+	public static double auCarre(double n){
+		return n*n;
+	}
 
-	d.arbres(2000,4);	
+	public static int deuxPuissance(int n){
+		int result = 1;
+		if(n>0){
+			for( int i = 0 ; i < n ; i++ ){
+				result = result*2;
+			}
+		}
+		if(n<=0){
+			return 1;
+		}
+		return result;
+	}
+
+	public static double calculDeFouDeLaLongueur(double l, int n){
+
+		double var1 = deuxPuissance(n)*l;
+		double var2 = deuxPuissance(n-1)*l;
+		double var3 = (deuxPuissance(n-1)-1)*l;
+
+
+		if(n==0 || n==1){
+			var1 = l;
+			var2 = l;
+			var3 = l;
+		}
+
+		double carre1 = auCarre(var1);
+		double carre2 = auCarre(var2);
+		double carre3 = auCarre(var3);
+
+
+		double a = carre1-carre2;
+		double racine1=Math.sqrt(a);
+		double b = racine1+carre3;
+
+		double racine2=Math.sqrt(b);
+
+		System.out.println("\nvar1 = " + var1 +
+				"\nvar2 = " + var2 +
+				"\nvar3 = " + var3 +
+				"\ncarre1 = " + carre1 +
+				"\ncarre2 = " + carre2 +
+				"\ncarre3 = " + carre3 +
+				"\nracine1 = " + racine1 +
+				"\nrésultat final : "+Double.toString(racine2)
+		);
+
+
+		return racine2;
+	}
+
+
+	/**
+	 * double l : longueur
+	 * int n    : ordre
+	 *
+	public void positionSierpinski(double l, int n){
+		if(n==0){
+			bob.forward(l);
+		}
+		if(n==1){
+			bob.right(60/n);
+			bob.forward(sqrt(auCarre(deuxPuissance(n)*l)-auCarre(l)));
+			bob.left(60/n);
+			bob.forward(sqrt(auCarre(deuxPuissance(n)*l)-auCarre(l)));
+		}
+		else{
+			positionSierpinski(l,n-1);
+
+			bob.right(60/n);
+			bob.forward(sqrt(sqrt(auCarre(deuxPuissance(n)*l)-auCarre(deuxPuissance(n-1)*l))+auCarre((deuxPuissance(n-1)-1)*l)));
+
+			positionSierpinski(l,n-1);
+
+			bob.left(60/n);
+			bob.forward(sqrt(sqrt(auCarre(deuxPuissance(n)*l)-auCarre(deuxPuissance(n-1)*l))+auCarre((deuxPuissance(n-1)-1)*l)));
+
+			positionSierpinski(l,n-1);
+		}
+	}
+
+	 */
+
+	/**
+	 * double l : longueur de l'arbre
+	 * int n    : l'ordre du triangle
+	 * */
+    public void Sierpinski(double l, int n){
+		if (n==0) {
+			triangle(l);
+		}
+		else {
+			while(n<0)
+
+			Sierpinski(l/2,n-1);
+
+			/**repositionnement*/
+			bob.right(60);
+			bob.left(60);
+			Sierpinski(l/2,n-1);
+			bob.left(120);
+			bob.forward(2l);
+			bob.left(60);
+			Sierpinski(l/2,n-1);
+
+
+		}
+	}
+
+
+
+/*____________________________________________________________________________ 
+*/
+
+    public static void main(String[] args){
+	//DessinFractale d = new DessinFractale(500);
+
+	//d.positionSierpinski(500,0);
+
+	calculDeFouDeLaLongueur(100,0);
+	calculDeFouDeLaLongueur(100,1);
+	calculDeFouDeLaLongueur(100,2);
+	calculDeFouDeLaLongueur(100,3);
+
     }
     
 }
